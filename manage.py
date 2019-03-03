a9dc4292ff61f4d8907b3c1a0543120aa25816a3
mostafaitalian <eng_mustafa_yossef@hotmail.com>
2019-03-03 15:38:41 +0200

    my first commit

--- manage.py
+++ manage.py
@@ -25,6 +25,6 @@ if __name__ == "__main__":
     # This allows easy placement of apps within the interior
     # cookie directory.
     current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(current_path, "cookie"))
+    sys.path.append(os.path.join(current_path, "cookie.settings"))
 
     execute_from_command_line(sys.argv)
